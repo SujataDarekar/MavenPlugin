012a7c5ffddf212119e7f496c3963a0ab840ffe1
sdarekar <sdarekar@tibco.com>
2017-10-27 15:38:25 +0530

    path changed

--- src/main/java/com/tibco/bw/maven/plugin/utils/Constants.java
+++ src/main/java/com/tibco/bw/maven/plugin/utils/Constants.java
@@ -27,6 +27,6 @@ public interface Constants {
 	public static final String XML_BOOLEAN = "xsd:boolean"; 	
 	public static final String BOOLEAN = "Boolean";
 	public static String ZipFilePath="/home/travis/build/SujataDarekar/MavenPlugin/src/test/java/TIB_BW_Maven_Plugin_1.2.2.zip";
-	public static String ExtractedZipFilePath="C://ZipFIleCreation//Unzip//install1//TIB_BW_Maven_Plugin_1.2.2";
+	public static String ExtractedZipFilePath="/home/travis/build/SujataDarekar/MavenPlugin/src/test/TIB_BW_Maven_Plugin_1.2.2";
 	
 }
